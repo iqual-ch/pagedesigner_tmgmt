256cf84676f4c7e20e0e856d7caf7bfba3d01d78
pvbergen <ph.vonbergen@iqual.ch>
2020-02-07 15:49:49 +0100

    Update title and link attributes

--- src/EventSubscriber/PagedesignerTranslateEventsSubscriber.php
+++ src/EventSubscriber/PagedesignerTranslateEventsSubscriber.php
@@ -2,10 +2,8 @@
 
 namespace Drupal\pagedesigner_tmgmt\EventSubscriber;
 
-use Drupal\Core\TempStore\SharedTempStore;
 use Drupal\pagedesigner\ElementEvents;
 use Drupal\pagedesigner\Event\ElementEvent;
-use Drupal\pagedesigner_content\PagedesignerItemProcessor;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -15,45 +13,62 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class PagedesignerTranslateEventsSubscriber implements EventSubscriberInterface {
 
-	/**
-	 * {@inheritdoc}
-	 *
-	 * @return array
-	 *   The event names to listen for, and the methods that should be executed.
-	 */
-	public static function getSubscribedEvents() {
-		return [
-			ElementEvents::COPY_AFTER => 'pagedesignerItemCopy',
-		];
-	}
+  /**
+   * {@inheritdoc}
+   *
+   * @return array
+   *   The event names to listen for, and the methods that should be executed.
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ElementEvents::COPY_AFTER => 'pagedesignerItemCopy',
+    ];
+  }
 
-	public $translation_data = null;
+  public $translation_data = NULL;
 
-	/**
-	 * React to a pagedesigner item being copied.
-	 *
-	 * @param \Drupal\pagedesigner\Event\ElementEvent $event
-	 *   Pagedesigner copy event.
-	 */
-	public function pagedesignerItemCopy(ElementEvent $event) {
-		$clone = $event->getData()[2];
-		$entity = $event->getData()[0];
-		$container  = $event->getData()[1];
+  /**
+   * React to a pagedesigner item being copied.
+   *
+   * @param \Drupal\pagedesigner\Event\ElementEvent $event
+   *   Pagedesigner copy event.
+   */
+  public function pagedesignerItemCopy(ElementEvent $event) {
+    $clone = $event->getData()[2];
+    $entity = $event->getData()[0];
+    $container = $event->getData()[1];
+    $handler = \Drupal::service('pagedesigner.service.element_handler');
     $store = \Drupal::service('user.shared_tempstore')->get('pagedesigner.tmgmt_data');
     $this->translation_data = $store->get($container->id());
 
-		if(isset($this->translation_data['pagedesigner_item'][$entity->id()])) {
-			if($this->translation_data['pagedesigner_item'][$entity->id()]['#translate']) {
-				$handler = \Drupal::service('pagedesigner.service.element_handler');
-				if (isset($this->translation_data['pagedesigner_item'][$entity->id()]['#translation']) && isset($this->translation_data['pagedesigner_item'][$entity->id()]['#translation']["#text"]))
-					$handler->patch($clone, [$this->translation_data['pagedesigner_item'][$entity->id()]['#translation']["#text"]]);
-			}
-		}
-		$clone->save();
-		/*var_dump('Dojdovne');
-		echo "</br></br>";
-		var_dump($clone );
-		die();*/
-	}
+    // Check for translation data.
+    if (isset($this->translation_data['pagedesigner_item'][$entity->id()])) {
+      $itemData = $this->translation_data['pagedesigner_item'][$entity->id()];
+      if ($itemData['#translate'] && isset($itemData['#translation']) && isset($itemData['#translation']["#text"])) {
+
+        $translatedText = $itemData['#translation']["#text"];
+
+        // Replace title attributes with translation.
+        if (isset($itemData['titles'])) {
+          foreach ($itemData['titles'] as $origTitle => $titleData) {
+            $translatedText = str_replace('title="' . $origTitle . '"', 'title="' . $titleData['#translation']['#text'] . '"', $translatedText);
+          }
+        }
+
+        // Update links to point to correct translation.
+        $hrefMatches = [];
+        preg_match('/\/href="(.*?)"/', $translatedText, $hrefMatches);
+        foreach ($hrefMatches as $hrefMatch) {
+          // Get id.
+          $id = getNodeId();
+          $translatedText = str_replace('href="' . $hrefMatch[1] . '"', 'href="' . $lang . '/node/' . $id . '"', $translatedText);
+        }
+
+        // Update the content of the element.
+        $handler->patch($clone, [$translatedText]);
+      }
+    }
+
+  }
 
 }
