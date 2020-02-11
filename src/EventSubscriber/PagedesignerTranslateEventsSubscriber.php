7891bba23a5ee18684a12d1e1c0f7bb9f6012f49
Ginovski <i.ginovski@iqual.ch>
2020-02-11 14:09:46 +0100

    Fixed redirect aliases

--- src/EventSubscriber/PagedesignerTranslateEventsSubscriber.php
+++ src/EventSubscriber/PagedesignerTranslateEventsSubscriber.php
@@ -5,6 +5,7 @@ namespace Drupal\pagedesigner_tmgmt\EventSubscriber;
 use Drupal\Core\Url;
 use Drupal\pagedesigner\ElementEvents;
 use Drupal\pagedesigner\Event\ElementEvent;
+use Drupal\redirect\Entity\Redirect;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -77,13 +78,41 @@ class PagedesignerTranslateEventsSubscriber implements EventSubscriberInterface 
             // language accordingly.
             $alias = \Drupal::service('path.alias_manager')->getPathByAlias($hrefMatch);
 
-            $params = Url::fromUri("internal:" . $alias)->getRouteParameters();
-            $entity_type = key($params);
-            if (isset($entity_type) && strlen($entity_type) > 0) {
-              $node = \Drupal::entityTypeManager()->getStorage($entity_type)->load($params[$entity_type]);
-              $id = $node->id();
-              $lang = $container->language()->getId();
-              $translatedText = str_replace($hrefMatches[0][$key], 'href="/' . $lang . '/node/' . $id . '"', $translatedText);
+            try {
+              // Check the redirect records.
+              if (!Url::fromUri('internal:' . $alias)->isRouted()) {
+                $query = \Drupal::entityQuery('redirect')
+                  ->range(0, 1);
+                $or = $query->orConditionGroup();
+                $or->condition('redirect_source__path', ltrim($alias,'/') . '%', 'LIKE');
+                $or->condition('redirect_source__path', substr($alias, 4) . '%', 'LIKE');
+                $query->condition($or);
+                $redirect = $query->execute();
+
+                if (isset($redirect) && $redirect) {
+                  $redirect = Redirect::load(reset($redirect));
+                  $alias = $redirect->get('redirect_redirect')->getValue()[0]['uri'];
+                }
+              }
+              else {
+                $alias = 'internal:' . $alias;
+              }
+
+              // Check if the alias is routed, otherwise it has no route
+              // parameters.
+              if (Url::fromUri($alias)->isRouted()) {
+                $params = Url::fromUri($alias)->getRouteParameters();
+                $entity_type = key($params);
+                if (isset($entity_type) && strlen($entity_type) > 0) {
+                  $node = \Drupal::entityTypeManager()->getStorage($entity_type)->load($params[$entity_type]);
+                  $id = $node->id();
+                  $lang = $container->language()->getId();
+                  $translatedText = str_replace($hrefMatches[0][$key], 'href="/' . $lang . '/node/' . $id . '"', $translatedText);
+                }
+              }
+            }
+            catch (\Exception $e) {
+              \Drupal::messenger()->addMessage($alias);
             }
           }
         }
